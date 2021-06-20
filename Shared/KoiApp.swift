121831e84da620a66629d8c6445bbe6ee718e821
Edgar Mejía <edgar13155@gmail.com>
2021-06-20 01:13:55 -0600

    Initial Commit

---
+++ Shared/KoiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KoiApp.swift
+//  Shared
+//
+//  Created by Edgar Mejía on 20/6/21.
+//
+
+import SwiftUI
+
+@main
+struct KoiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
