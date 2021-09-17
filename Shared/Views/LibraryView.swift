c96ce53f5922feee0cb45e6f850ae910102122a2
Edgar Mejía <edgar13155@gmail.com>
2021-09-17 15:37:10 -0600

    Add categories controller.

--- Shared/Views/LibraryView.swift
+++ Shared/Views/LibraryView.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 struct LibraryView: View {
     
+    @State private var favoriteColor = "Red"
+    
+    var colors = ["Reading", "Planned", "NSFW"]
+    
     var body: some View {
         content
     }
@@ -16,13 +20,32 @@ struct LibraryView: View {
     var content: some View {
         ScrollView {
             VStack(spacing: 0) {
+                Picker("", selection: $favoriteColor) {
+                    ForEach(colors, id: \.self) {
+                        Text($0)
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                .padding()
+                
                 MangaGrid()
                     .padding()
             }
+//            .toolbar {
+//                ToolbarItem(placement: .principal) {
+//                    Picker("", selection: $favoriteColor) {
+//                        ForEach(colors, id: \.self) {
+//                            Text($0)
+//                        }
+//                    }
+//                    .pickerStyle(SegmentedPickerStyle())
+//                }
+//            }
             .navigationTitle("Library")
         }
     }
 }
+
 
 struct LibraryView_Previews: PreviewProvider {
     static var previews: some View {
